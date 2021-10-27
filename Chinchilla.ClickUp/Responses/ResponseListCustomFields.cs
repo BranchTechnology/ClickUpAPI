cc4260580cd1675ee99d5c90f0605e71a20feccf
Jake <jake@branch.technology>
2021-10-27 15:51:40 -0400

    added CustomField to ResponseListCustomField

--- Chinchilla.ClickUp/Responses/ResponseListCustomFields.cs
+++ Chinchilla.ClickUp/Responses/ResponseListCustomFields.cs
@@ -1,6 +1,7 @@
 using Newtonsoft.Json;
 using Chinchilla.ClickUp.Responses.Model;
 using System.Collections.Generic;
+using Chinchilla.ClickUp.CustomFields;
 
 namespace Chinchilla.ClickUp.Responses
 {
@@ -15,6 +16,7 @@ namespace Chinchilla.ClickUp.Responses
 		/// List of CustomField Model with information of the List
 		/// </summary>
 		[JsonProperty("fields")]
-		public List<ResponseModelListCustomField> Fields { get; set; }
+		//public List<ResponseModelListCustomField> Fields { get; set; }
+        public List<CustomField> Fields { get; set; }
 	}
 }
